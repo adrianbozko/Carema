fcc811bc74466b0d493c10d6ad31e30ea319fc3e
Adrian Bozko <gequpl@gmail.com>
2023-09-19 14:18:41 +0200

    added title, meta description and keywords to all sites, small fixes

--- form.php
+++ form.php
@@ -5,8 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dziękujemy! - Carema</title>
+    <title>Dziękujemy! - CAREMA | Szukasz pracy w Niemczech? Już znalazłaś!</title>
+    <meta name="description" content="Dołącz do nas i zostań Opiekunką seniorów! Legalna praca, pełne wsparcie, prosta rekrutacja i wysokie zarobki. Sprawdź sama!">
+    <meta name="keywords" content="Praca w opiece, Praca w opiece w Niemczach, Praca w opiece Niemcy, Opieka nad seniorami Niemcy, Opiekunka seniorów, Opiekunka seniorów w Niemczech, Praca w Niemczech, Praca jako opiekunka, Opieka nad seniorami, Opiekunka osób starszych, Opiekunka osób starszych praca Niemcy, Opiekunka oferty pracy Niemcy, Praca dla opiekunek w Niemczech"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-touch-icon.png?v=1">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png?v=1">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png?v=1">
+    <link rel="manifest" href="favicon/site.webmanifest?v=1">
+    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg?v=1" color="#5bbad5">
+    <link rel="shortcut icon" href="favicon/favicon.ico?v=1">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="favicon/browserconfig.xml?v=1">
+    <meta name="theme-color" content="#ffffff">
 </head>
 
 <body>
